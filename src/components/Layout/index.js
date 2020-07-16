54eff1b12c67a23037bf7ca954f17558a471f051
leandro_matos <59984034+leandro-matos@users.noreply.github.com>
2020-07-16 00:30:53 -0300

    Alterando layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,3 @@
-
 import React from "react"
 import PropTypes from "prop-types"
 import { TransitionPortal } from 'gatsby-plugin-transition-link'
--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -1,10 +1,8 @@
-
 import styled from 'styled-components'
 import media from 'styled-media-query'
 
 export const LayoutWrapper = styled.section`
   display: flex;
-
   ${media.lessThan("large")`
     flex-direction: column;
   `}
@@ -15,13 +13,11 @@ export const LayoutMain = styled.main`
   padding: 0 3.75rem 0 20rem;
   transition: background, color 0.5s;
   width: 100%;
-
   body#grid & {
     grid-template-areas: 
     "posts" 
     "pagination";
   }
-
   ${media.lessThan("large")`
     padding: 4.125rem 0 3rem 0;
   `}
