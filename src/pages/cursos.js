ffa98862160fc817deee3110de17aa5dd3ea1f7a
leandro_matos <59984034+leandro-matos@users.noreply.github.com>
2020-07-18 19:15:45 -0300

    Atualizando cursos

--- src/pages/cursos.js
+++ src/pages/cursos.js
@@ -20,16 +20,16 @@ const Cursos = () => (
         <li><a href="https://drive.google.com/open?id=1VyDaxP63QPL-SqUG81Caf6R8AbIpeOM7"
             target="_blank">Comptia Linux+ (Powered By LPI)</a></li>
 
-        <li><a href="https://drive.google.com/open?id=1VyDaxP63QPL-SqUG81Caf6R8AbIpeOM7"
+        <li><a href="https://drive.google.com/open?id=1_5JECa9ylIEITR82FGvGR1ohe2u5Yfv4"
             target="_blank">Linux Professional Institute - LPIC1</a></li>
 
-        <li><a href="https://drive.google.com/open?id=1VyDaxP63QPL-SqUG81Caf6R8AbIpeOM7"
+        <li><a href="http://lpi.org/v/LPI000355631/fk5dz7z8et"
             target="_blank">Linux Professional Institute - Linux Essentials</a></li>
 
-        <li><a href="https://drive.google.com/open?id=1VyDaxP63QPL-SqUG81Caf6R8AbIpeOM7"
+        <li><a href="https://drive.google.com/open?id=0B8E-_axUFefdZDcxR2swUHZETWc"
             target="_blank">Comptia IT Fundamentals</a></li>
 
-        <li><a href="https://drive.google.com/open?id=1VyDaxP63QPL-SqUG81Caf6R8AbIpeOM7"
+        <li><a href="https://drive.google.com/open?id=0B8E-_axUFefdcW5mY2R2YWVmdkU"
             target="_blank">MTA - Microsoft Technology Associate: 98-366 Networking Fundamentals</a></li>
 
       </ul>
